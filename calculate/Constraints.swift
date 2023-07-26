99f2a14a1ebab60017dac2b92a279026d2748670
Aker <@akerke.n@inbox.ru>
2023-07-26 14:19:49 +0600

    logic

---
+++ calculate/Constraints.swift
@@ -0,0 +1,126 @@
+//
+//  Constraints.swift
+//  calculate
+//
+//  Created by Akerke on 26.07.2023.
+//
+
+import Foundation
+import SnapKit
+import UIKit
+
+
+func makeConstraints() {
+    resultView.snp.makeConstraints{
+        $0.height.equalTo(350)
+        $0.width.equalTo(400)
+        $0.centerX.equalToSuperview()
+        $0.centerY.equalToSuperview().offset(-180)
+    }
+    showingLabel.snp.makeConstraints{
+        $0.left.right.equalToSuperview().offset(25)
+        $0.centerY.equalTo(resultView).offset(-90)
+    }
+    resultLabel.snp.makeConstraints{
+        $0.left.right.equalToSuperview().offset(25)
+        $0.centerY.equalTo(resultView).offset(100)
+    }
+    firstButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-150)
+        $0.centerY.equalToSuperview().offset(250)
+    }
+    secondButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-60)
+        $0.centerY.equalToSuperview().offset(250)
+    }
+    thirdButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(30)
+        $0.centerY.equalToSuperview().offset(250)
+    }
+    zeroButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-60)
+        $0.centerY.equalToSuperview().offset(350)
+        
+    }
+    fourthButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-150)
+        $0.centerY.equalToSuperview().offset(150)
+    }
+    fifthButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-60)
+        $0.centerY.equalToSuperview().offset(150)
+    }
+    sixthButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(30)
+        $0.centerY.equalToSuperview().offset(150)
+    }
+    seventhButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-150)
+        $0.centerY.equalToSuperview().offset(50)
+    }
+    eighthButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-60)
+        $0.centerY.equalToSuperview().offset(50)
+    }
+    ninthButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(30)
+        $0.centerY.equalToSuperview().offset(50)
+    }
+    ceButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(-150)
+        $0.centerY.equalToSuperview().offset(350)
+        
+    }
+    equalsButton.snp.makeConstraints{
+        $0.width.equalTo(80)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(30)
+        $0.centerY.equalToSuperview().offset(350)
+    }
+    pluseButton.snp.makeConstraints{
+        $0.width.equalTo(100)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(135)
+        $0.centerY.equalToSuperview().offset(350)
+    }
+    minusButton.snp.makeConstraints{
+        $0.width.equalTo(100)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(135)
+        $0.centerY.equalToSuperview().offset(250)
+    }
+    divideButton.snp.makeConstraints{
+        $0.width.equalTo(100)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(135)
+        $0.centerY.equalToSuperview().offset(150)
+    }
+    multiplyButton.snp.makeConstraints{
+        $0.width.equalTo(100)
+        $0.height.equalTo(80)
+        $0.centerX.equalToSuperview().offset(135)
+        $0.centerY.equalToSuperview().offset(50)
+    }
+}
--- calculate/ViewController.swift
+++ calculate/ViewController.swift
@@ -14,6 +14,7 @@ enum Operations: Int {
     case minus = 100
     case divede = 101
     case multiply = 102
+   
 }
 
 class ViewController: UIViewController {
@@ -22,223 +23,11 @@ class ViewController: UIViewController {
         didSet { resultLabel.text = String(Int(currentNumber) ?? 0) }
     }
     var firstOperand: Double = 0
-    var operatorSelected: String = ""
-    var isTypingNumber: Bool = false
     var tempNumber: String = ""
-    var currentOperation: Operations?
-    
-    let resultView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemIndigo
-        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMaxYCorner]
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 50
-        return view
-    }()
-    
-    let resultLabel: UILabel = {
-        let label = UILabel()
-        label.text = "0"
-        label.numberOfLines = 1
-        label.adjustsFontSizeToFitWidth = true
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 90)
-        return label
-    }()
-    
-    let zeroButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("0", for: .normal)
-        button.tag = 0
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let firstButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("1", for: .normal)
-        button.titleLabel?.textColor = .red
-        button.tag = 1
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let secondButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("2", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 2
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    
-    let thirdButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("3", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 3
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    
-    let fourthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("4", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 4
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let fifthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("5", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 5
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let sixthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("6", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.tag = 6
-        button.clipsToBounds = true
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        //        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let seventhButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("7", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 7
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let eighthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("8", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 8
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let ninthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemIndigo
-        button.setTitle("9", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
-        button.tag = 9
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let ceButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("CE", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(clearButtonPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let equalsButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("=", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(36)
-        //        button.addTarget(self, action: #selector(equalsButtonPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let pluseButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("+", for: .normal)
-        button.clipsToBounds = true
-        button.tag = Operations.pluse.rawValue
-        button.titleLabel?.font = button.titleLabel?.font.withSize(24)
-        button.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let minusButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("-", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(48)
-        button.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.tag = Operations.minus.rawValue
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let divideButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("/", for: .normal)
-        button.clipsToBounds = true
-        button.tag = Operations.divede.rawValue
-        button.titleLabel?.font = button.titleLabel?.font.withSize(36)
-        button.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
-        button.layer.cornerRadius = 40
-        return button
-    }()
-    
-    let multiplyButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemMint
-        button.setTitle("*", for: .normal)
-        button.titleLabel?.font = button.titleLabel?.font.withSize(48)
-        button.tag = Operations.multiply.rawValue
-        button.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 40
-        return button
-    }()
+    var currentOperation: Operations? = nil
+    var tempOperation: Operations? = nil
+    var showingNumbers: String = ""
+       
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -269,119 +58,24 @@ private extension ViewController {
         view.addSubview(minusButton)
         view.addSubview(divideButton)
         view.addSubview(multiplyButton)
+        resultView.addSubview(showingLabel)
+        zeroButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        firstButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        secondButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        thirdButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        fourthButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        fifthButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        sixthButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        seventhButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        eighthButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        ninthButton.addTarget(self, action: #selector(digitButtonPressed), for: .touchUpInside)
+        ceButton.addTarget(self, action: #selector(clearButtonPressed), for: .touchUpInside)
+        equalsButton.addTarget(self, action: #selector(equalsButtonPressed), for: .touchUpInside)
+        pluseButton.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
+        minusButton.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
+        divideButton.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
+        multiplyButton.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
         
-    }
-    
-    func makeConstraints() {
-        resultView.snp.makeConstraints{
-            $0.height.equalTo(350)
-            $0.width.equalTo(400)
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview().offset(-200)
-        }
-        resultLabel.snp.makeConstraints{
-            $0.height.equalTo(100)
-            $0.left.right.equalToSuperview().inset(25)
-            $0.centerY.equalTo(resultView).offset(100)
-        }
-        firstButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-150)
-            $0.centerY.equalToSuperview().offset(250)
-        }
-        secondButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-60)
-            $0.centerY.equalToSuperview().offset(250)
-        }
-        thirdButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(30)
-            $0.centerY.equalToSuperview().offset(250)
-        }
-        zeroButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-60)
-            $0.centerY.equalToSuperview().offset(350)
-            
-        }
-        fourthButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-150)
-            $0.centerY.equalToSuperview().offset(150)
-        }
-        fifthButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-60)
-            $0.centerY.equalToSuperview().offset(150)
-        }
-        sixthButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(30)
-            $0.centerY.equalToSuperview().offset(150)
-        }
-        seventhButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-150)
-            $0.centerY.equalToSuperview().offset(50)
-        }
-        eighthButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-60)
-            $0.centerY.equalToSuperview().offset(50)
-        }
-        ninthButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(30)
-            $0.centerY.equalToSuperview().offset(50)
-        }
-        ceButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(-150)
-            $0.centerY.equalToSuperview().offset(350)
-            
-        }
-        equalsButton.snp.makeConstraints{
-            $0.width.equalTo(80)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(30)
-            $0.centerY.equalToSuperview().offset(350)
-        }
-        pluseButton.snp.makeConstraints{
-            $0.width.equalTo(100)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(130)
-            $0.centerY.equalToSuperview().offset(350)
-        }
-        minusButton.snp.makeConstraints{
-            $0.width.equalTo(100)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(130)
-            $0.centerY.equalToSuperview().offset(250)
-        }
-        divideButton.snp.makeConstraints{
-            $0.width.equalTo(100)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(130)
-            $0.centerY.equalToSuperview().offset(150)
-        }
-        multiplyButton.snp.makeConstraints{
-            $0.width.equalTo(100)
-            $0.height.equalTo(80)
-            $0.centerX.equalToSuperview().offset(130)
-            $0.centerY.equalToSuperview().offset(50)
-        }
     }
     
     @objc func zeroTapped() {
@@ -389,26 +83,38 @@ private extension ViewController {
             if let text = resultLabel.text {
                 resultLabel.text = "\(text)\(0)"
             }
+            addToShowingNumbers(value: "0")
         }
     }
     
     @objc func digitButtonPressed(sender: Any) {
-        guard let button = sender as? UIButton else {return}
-        currentNumber += String(button.tag)
+        guard let button = sender as? UIButton else { return }
         
+        if currentOperation != nil {
+            tempNumber = currentNumber
+            currentNumber = String(button.tag)
+            print(currentNumber)
+            tempOperation = currentOperation
+            currentOperation = nil
+        } else {
+            currentNumber += String(button.tag)
+        }
+        addToShowingNumbers(value: "\(button.tag)")
     }
     
     @objc func clearButtonPressed() {
         currentNumber = "0"
-        
+        showingNumbers = ""
+        showingLabel.text = ""
     }
-    @objc func equalButtonPressed() {
-        guard let currentOperation = currentOperation,
-              let left = Int(tempNumber),
-              let right = Int(currentNumber) else {return}
-        let resultNumber: Int
         
-        switch currentOperation {
+    @objc func equalsButtonPressed() {
+        guard let tempOperation = tempOperation,
+              let left = Double(tempNumber),
+              let right = Double(currentNumber) else {return}
+        let resultNumber: Double
+        
+        switch tempOperation {
         case .pluse:
             resultNumber = left + right
         case .minus:
@@ -417,17 +123,49 @@ private extension ViewController {
             resultNumber = left / right
         case .multiply:
             resultNumber = left * right
+    
         }
+        
         currentNumber = "\(resultNumber)"
+        tempNumber = ""
+        
+        resultLabel.text = String(resultNumber)
+        self.tempOperation = nil
+        self.currentOperation = nil
+        addToShowingNumbers(value:" = \(resultNumber)")
         
     }
     
     @objc func operationPressed(sender: Any) {
-        guard let button = sender as? UIButton else {return} 
+        
+        guard let button = sender as? UIButton else {return}
         let operation = Operations(rawValue: button.tag)
+        var convertedOperation: String = ""
+        
+        switch operation {
+        case .pluse:
+            convertedOperation = "+"
+        case .minus:
+            convertedOperation = "-"
+        case .divede:
+            convertedOperation =  "/"
+        case .multiply:
+            convertedOperation = "*"
+            
+      
+        case .none:
+            print("none")
+        }
+        
         currentOperation = operation
         tempNumber = currentNumber
+        addToShowingNumbers(value: "\(convertedOperation)")
+
+    }
+    
+    func addToShowingNumbers(value: String){
+        showingNumbers = showingNumbers + value
+        showingLabel.text = showingNumbers
         
     }
 }
-
